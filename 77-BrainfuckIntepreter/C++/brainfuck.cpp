5e81abd11b407d85afe3bce8668bc64dcc150e0d
daing100 <carl.fredrik.lidgren@gmail.com>
2017-01-09 02:18:49 +0100

    Additional options
    Added more options to the brainfuck parser, such as the inclusion of the
    BF command at the end of the program.

--- 77-BrainfuckIntepreter/C++/brainfuck.cpp
+++ 77-BrainfuckIntepreter/C++/brainfuck.cpp
@@ -24,13 +24,27 @@ int main(){
 	char command[16384] = {0};
 	cin >> command;
 
+
+	cout << "\nPrint command at the end of the program? [y/n] ";
+	char keepCommand;
+	cin >> keepCommand;
+
 	cout << "\nWriting file...\n";
 
 	file << "#include <stdio.h>\n#include <iostream>\nusing namespace std;\n";
 	file << "\nint main(){\n";
 	file << "uint8_t cells[512] = {0};\nuint8_t *ptr = cells;\n";
 
+	if(keepCommand == 'y'){
+		file << "char command[] = \"" << command << "\";\n";
+	}
+
+	int indentCounter = 0;
+
 	for(char &c : command){
+		for (int i = 0; i < indentCounter; i++){
+			file << "\t";
+		}
 		if(c == '>') {
 			file << "\t++ptr;\n";
 		} else if(c == '<'){
@@ -42,15 +56,20 @@ int main(){
 		} else if(c == '.'){
 			file << "\tputchar(*ptr);\n";
 		} else if(c == ','){
-			file << "\t*ptr = getchar();\n";
+			file << "\t*ptr = getchar();\n\tgetchar(); // To take the newline that appears after entering a char.\n";
 		} else if(c == '['){
+			indentCounter++;
 			file << "\twhile (*ptr) {\n";
 		} else if(c == ']'){
-			file << "\t}\n";
+			indentCounter--;
+			file << "}\n";
 		}
 	}
 	if(cell_bug == 'y'){
 		file << "\tfor(int i = 0; i < " << numberOfCells * 4 << "; i += 4){\n\t\tcout << (int) (cells[i]) << \" \";\n\t\tcout << (int) (cells[i+1]) << \" \";\n\t\tcout << (int) (cells[i+2]) << \" \";\n\t\tcout << (int) (cells[i+3]) << \"\\n\";\n\t}\n";
+	}
+	if(keepCommand == 'y'){
+		file << "\tcout << command;\n";
 	}
 	file << "}";
 	file.close();
@@ -59,7 +78,14 @@ int main(){
 	cin >> te;
 	if(te == 'y'){
 		system(("g++ -o " + name + ".exe" + " " + name + ".cpp").c_str());
+		cout << "Run now? [y/n] ";
+		char ans;
+		cin >> ans;
+		if(ans == 'y'){
+			system((name + ".exe").c_str());
+		}
 	}
+
 	return 0;
 }
 
